71db45258e34b601a820a0bec41067e4f191196b
Felix Pojtinger <felix@pojtinger.com>
2020-09-15 16:42:51 +0200

    feat: Add basic boilerplate

---
+++ cmd/liwasc-frontend-web-server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/maxence-charriere/go-app/v7/pkg/app"
+)
+
+func main() {
+	listenAddress := flag.String("listenAddress", "0.0.0.0:7000", "Listen address")
+
+	flag.Parse()
+
+	h := app.Handler{
+		Title:  "liwasc",
+		Author: "Felix Pojtinger",
+		Styles: []string{"https://unpkg.com/@patternfly/patternfly@4.35.2/patternfly.css"},
+	}
+
+	log.Println("Listening on", *listenAddress)
+
+	if err := http.ListenAndServe(*listenAddress, &h); err != nil {
+		log.Fatal("could not start server", err)
+	}
+}
