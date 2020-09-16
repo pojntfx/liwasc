58c13f7938c4fb44cd2d925e68877744c63635b4
Felix Pojtinger <felix@pojtinger.com>
2020-09-16 21:34:24 +0200

    feat: Add table of nodes

--- pkg/components/app.go
+++ pkg/components/app.go
@@ -9,5 +9,31 @@ type AppComponent struct {
 func (c *AppComponent) Render() app.UI {
 	return app.Div().Body(
 		&FilterComponent{Subnets: []string{"10.0.0.0/9", "192.168.0.0/27"}, Device: "eth0"},
+		&ListingComponent{
+			Nodes: []ListingNode{
+				{
+					PoweredOn:  false,
+					MACAddress: "ff:ff:ff:ff",
+					IPAddress:  "10.0.0.1",
+					Vendor:     "TP-Link",
+					ServicesAndPorts: []string{
+						"22/tcp (ssh)",
+						"53/udp (dns)",
+						"80/tcp (http)",
+					},
+				},
+				{
+					PoweredOn:  true,
+					MACAddress: "00:1B:44:11:3A:B7",
+					IPAddress:  "10.0.0.2",
+					Vendor:     "Realtek",
+					ServicesAndPorts: []string{
+						"7/tcp (echo)",
+						"7/tcp (echo)",
+						"80/tcp (http)",
+					},
+				},
+			},
+		},
 	)
 }
