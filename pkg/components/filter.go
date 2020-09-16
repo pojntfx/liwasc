dd07acb68b368aa48da2bd38dd05e1123b93eb6c
Felix Pojtinger <felix@pojtinger.com>
2020-09-16 18:48:05 +0200

    feat: Add basic subnets and device display

--- pkg/components/filter.go
+++ pkg/components/filter.go
@@ -1,10 +1,13 @@
 package components
 
-import "github.com/maxence-charriere/go-app/v7/pkg/app"
+import (
+	"github.com/maxence-charriere/go-app/v7/pkg/app"
+)
 
 type FilterComponent struct {
 	app.Compo
 	Subnets []string
+	Device  string
 }
 
 func (c *FilterComponent) Render() app.UI {
@@ -12,23 +15,10 @@ func (c *FilterComponent) Render() app.UI {
 		app.Div().Class("pf-c-toolbar__content").Body(
 			app.Div().Class("pf-c-toolbar__content-section").Body(
 				app.Div().Class("pf-c-toolbar__item").Body(
-					app.Div().Class("pf-c-label-group pf-m-category").Body(
-						app.Span().Class("pf-c-label-group__label").Body(
-							app.I().Class("fas fa-network-wired pf-u-mr-xs"),
-							app.Text("Subnets"),
-						),
-						app.Ul().Class("pf-c-label-group__list").Body(
-							app.Range(c.Subnets).Slice(func(i int) app.UI {
-								return app.Li().Class("pf-c-label-group__list-item").Body(
-									app.Span().Class("pf-c-label").Body(
-										app.Span().Class("pf-c-label__content").Body(
-											app.Text(c.Subnets[i]),
-										),
-									),
-								)
-							}),
-						),
-					),
+					&LabelCollectionComponent{Icon: "fas fa-network-wired", Title: "Subnets", Labels: c.Subnets},
+				),
+				app.Div().Class("pf-c-toolbar__item").Body(
+					&LabelCollectionComponent{Icon: "fas fa-microchip", Title: "Device", Labels: []string{c.Device}},
 				),
 				app.Div().Class("pf-c-toolbar__item pf-m-pagination").Body(
 					app.Button().Class("pf-c-button pf-m-primary").Body(
