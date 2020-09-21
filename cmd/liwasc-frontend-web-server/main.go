1c9bb92766ca474109447ab3ec806eec6abae0ae
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-22 00:17:05 +0200

    feat: Add full PWA functionality

--- cmd/liwasc-frontend-web-server/main.go
+++ cmd/liwasc-frontend-web-server/main.go
@@ -14,13 +14,42 @@ func main() {
 	flag.Parse()
 
 	h := app.Handler{
-		Title:  "liwasc",
-		Author: "Felicitas Pojtinger",
+		Author:          "Felicitas Pojtinger",
+		BackgroundColor: "#151515",
+		Description:     "List, wake and scan nodes in a network.",
+		Icon: app.Icon{
+			Default: "/web/icon.png",
+		},
+		Keywords: []string{
+			"network",
+			"network-scanner",
+			"port-scanner",
+			"ip-scanner",
+			"arp-scanner",
+			"arp",
+			"iana",
+			"ports2packets",
+			"liwasc",
+			"vendor2mac",
+			"wake-on-lan",
+			"wol",
+			"service-name",
+		},
+		LoadingLabel: "List, wake and scan nodes in a network.",
+		Name:         "liwasc",
+		RawHeaders: []string{
+			`<meta property="og:url" content="https://liwasc.felicitas.pojtinger.com">`,
+			`<meta property="og:title" content="liwasc">`,
+			`<meta property="og:description" content="List, wake and scan nodes in a network.">`,
+			`<meta property="og:image" content="https://liwasc.felicitas.pojtinger.com/web/icon.png">`,
+		},
 		Styles: []string{
 			"https://unpkg.com/@patternfly/patternfly@4.42.2/patternfly.css",
 			"https://unpkg.com/@patternfly/patternfly@4.42.2/patternfly-addons.css",
 			"/web/index.css",
 		},
+		ThemeColor: "#151515",
+		Title:      "liwasc",
 	}
 
 	log.Println("Listening on", *listenAddress)
