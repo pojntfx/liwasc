49e399887abd98b53e7bf5c631a334b6c35c9a43
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-28 21:12:13 +0200

    feat: Enable getting the newest node by specifiying -1 as the node ID

--- pkg/sql/generated/liwasc/scans_nodes.go
+++ pkg/sql/generated/liwasc/scans_nodes.go
@@ -45,27 +45,6 @@ var ScansNodeColumns = struct {
 
 // Generated where
 
-type whereHelpertime_Time struct{ field string }
-
-func (w whereHelpertime_Time) EQ(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.EQ, x)
-}
-func (w whereHelpertime_Time) NEQ(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.NEQ, x)
-}
-func (w whereHelpertime_Time) LT(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.LT, x)
-}
-func (w whereHelpertime_Time) LTE(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.LTE, x)
-}
-func (w whereHelpertime_Time) GT(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.GT, x)
-}
-func (w whereHelpertime_Time) GTE(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.GTE, x)
-}
-
 var ScansNodeWhere = struct {
 	ID        whereHelperint64
 	CreatedAt whereHelpertime_Time
