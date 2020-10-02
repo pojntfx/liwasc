12fe1d5034c6a9dc5ce932d7c5d37659a97c136e
Felix Pojtinger <felix@pojtinger.com>
2020-10-02 23:23:05 +0200

    feat: Add basic node wake database functionality and start implementing service

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	deviceName := flag.String("deviceName", "eth0", "Network device name")
 	mac2vendorDatabasePath := flag.String("mac2vendorDatabasePath", "/etc/liwasc/oui-database.sqlite", "Path to the mac2vendor database. Download from https://mac2vendor.com/articles/download")
 	networkAndNodeScanDatabasePath := flag.String("networkAndNodeScanDatabasePath", "/var/liwasc/network_and_node_scan.sqlite", "Path to the persistence database for the network and node scan service.")
+	nodeWakeDatabasePath := flag.String("nodeWakeDatabasePath", "/var/liwasc/node_wake.sqlite", "Path to the persistence database for the node wake service.")
 	serviceNamesPortNumbersDatabasePath := flag.String("serviceNamesPortNumbersDatabasePath", "/etc/liwasc/service-names-port-numbers.csv", "Path to the CSV input file containing the registered services. Download from https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml")
 	ports2PacketsDatabasePath := flag.String("ports2PacketsDatabasePath", "/etc/liwasc/ports2packets.csv", "Path to the ports2packets database. Download from https://github.com/pojntfx/ports2packets/releases")
 	listenAddress := flag.String("listenAddress", "0.0.0.0:15123", "Listen address.")
@@ -25,6 +26,7 @@ func main() {
 	// Create instances
 	mac2VendorDatabase := databases.NewMAC2VendorDatabase(*mac2vendorDatabasePath)
 	networkAndNodeScanDatabase := databases.NewNetworkAndNodeScanDatabase(*networkAndNodeScanDatabasePath)
+	nodeWakeDatabase := databases.NewNodeWakeDatabase(*nodeWakeDatabasePath)
 	serviceNamesPortNumbersDatabase := databases.NewServiceNamesPortNumbersDatabase(*serviceNamesPortNumbersDatabasePath)
 	ports2PacketsDatabase := databases.NewPorts2PacketDatabase(*ports2PacketsDatabasePath)
 	networkAndNodeScanService := services.NewNetworkAndNodeScanService(
@@ -35,7 +37,19 @@ func main() {
 		networkAndNodeScanDatabase,
 		semaphore.NewWeighted(int64(*maxConcurrentPortScans)),
 	)
-	liwascServer := servers.NewLiwascServer(*listenAddress, networkAndNodeScanService)
+	nodeWakeService := services.NewNodeWakeService(
+		*deviceName,
+		nodeWakeDatabase,
+		func(macAddress string) (string, error) {
+			node, err := networkAndNodeScanDatabase.GetNode(macAddress)
+			if err != nil {
+				return "", err
+			}
+
+			return node.IPAddress, nil
+		},
+	)
+	liwascServer := servers.NewLiwascServer(*listenAddress, networkAndNodeScanService, nodeWakeService)
 
 	// Open instances
 	if err := mac2VendorDatabase.Open(); err != nil {
@@ -52,6 +66,10 @@ func main() {
 
 	if err := networkAndNodeScanDatabase.Open(); err != nil {
 		log.Fatal("could not open networkAndNodeScanDatabase", err)
+	}
+
+	if err := nodeWakeDatabase.Open(); err != nil {
+		log.Fatal("could not open nodeWakeDatabase", err)
 	}
 
 	log.Printf("Listening on %v", *listenAddress)
--- pkg/services/node_wake.go
+++ pkg/services/node_wake.go
@@ -1,1 +1,42 @@
 package services
+
+import (
+	"context"
+	"log"
+
+	cmap "github.com/orcaman/concurrent-map"
+	"github.com/pojntfx/liwasc/pkg/databases"
+	proto "github.com/pojntfx/liwasc/pkg/proto/generated"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type NodeWakeService struct {
+	proto.UnimplementedNodeWakeServiceServer
+
+	device             string
+	nodeWakeDatabase   *databases.NodeWakeDatabase
+	nodeWakeScanners   cmap.ConcurrentMap
+	nodeWakeMessengers cmap.ConcurrentMap
+	getIPAddress       func(string) (string, error)
+}
+
+func NewNodeWakeService(
+	device string,
+	nodeWakeDatabase *databases.NodeWakeDatabase,
+	getIPAddress func(string) (string, error),
+) *NodeWakeService {
+	return &NodeWakeService{
+		device:             device,
+		nodeWakeDatabase:   nodeWakeDatabase,
+		nodeWakeScanners:   cmap.New(),
+		nodeWakeMessengers: cmap.New(),
+		getIPAddress:       getIPAddress,
+	}
+}
+
+func (s *NodeWakeService) TriggerNodeWake(ctx context.Context, nodeWakeTriggerMessage *proto.NodeWakeTriggerMessage) (*proto.NodeWakeReferenceMessage, error) {
+	log.Println(nodeWakeTriggerMessage)
+
+	return nil, status.Errorf(codes.Unimplemented, "method TriggerNodeWake not implemented")
+}
