2a7a952865554169cacb951bee19d4f56da56314
Felix Pojtinger <felix@pojtinger.com>
2020-10-05 00:44:24 +0200

    feat: Add placeholders for service inspector

--- pkg/components/service_inspector.go
+++ pkg/components/service_inspector.go
@@ -18,53 +18,101 @@ func (c *ServiceInspectorComponent) Render() app.UI {
 		app.Div().Class("pf-u-mb-lg").Text(c.Service.Description),
 		app.Dl().Class("pf-c-description-list pf-m-2-col pf-u-mb-md").Body(
 			&DefinitionComponent{
-				Title:   "Assignee",
-				Icon:    "fas fa-user-circle",
-				Content: &SearchLinkComponent{Topic: c.Service.Assignee},
+				Title: "Assignee",
+				Icon:  "fas fa-user-circle",
+				Content: func() app.UI {
+					if c.Service.Assignee != "" {
+						return &SearchLinkComponent{Topic: c.Service.Assignee}
+					}
+
+					return app.Text("Unknown assignee")
+				}(),
 			},
 			&DefinitionComponent{
 				Title: "Contact",
 				Icon:  "fas fa-paper-plane",
-				Content: app.A().
-					Href(fmt.Sprintf("mailto:%v", c.Service.Contact)).
-					Text(c.Service.Contact),
+				Content: func() app.UI {
+					if c.Service.Contact != "" {
+						return app.A().
+							Href(fmt.Sprintf("mailto:%v", c.Service.Contact)).
+							Text(c.Service.Contact)
+					}
+
+					return app.Text("Unknown contact")
+				}(),
 			},
 		),
 		app.Dl().Class("pf-c-description-list pf-m-2-col pf-u-mb-md").Body(
 			&DefinitionComponent{
-				Title:   "Registration Date",
-				Icon:    "fas fa-calendar-plus",
-				Content: app.Text(c.Service.RegistrationDate),
+				Title: "Registration Date",
+				Icon:  "fas fa-calendar-plus",
+				Content: app.Text(func() string {
+					if c.Service.RegistrationDate != "" {
+						return c.Service.RegistrationDate
+					}
+
+					return "Unknown registration date"
+				}()),
 			},
 			&DefinitionComponent{
-				Title:   "Modification Date",
-				Icon:    "fas fa-pen-square",
-				Content: app.Text(c.Service.ModificationDate),
+				Title: "Modification Date",
+				Icon:  "fas fa-pen-square",
+				Content: app.Text(func() string {
+					if c.Service.ModificationDate != "" {
+						return c.Service.ModificationDate
+					}
+
+					return "Unknown modification date"
+				}()),
 			},
 		),
 		app.Dl().Class("pf-c-description-list pf-m-2-col pf-u-mb-md").Body(
 			&DefinitionComponent{
-				Title:   "Reference",
-				Icon:    "fas fa-book-open",
-				Content: &SearchLinkComponent{Topic: c.Service.Reference},
+				Title: "Reference",
+				Icon:  "fas fa-book-open",
+				Content: func() app.UI {
+					if c.Service.Reference != "" {
+						return &SearchLinkComponent{Topic: c.Service.Reference}
+					}
+
+					return app.Text("Unknown reference")
+				}(),
 			},
 			&DefinitionComponent{
-				Title:   "Service Code",
-				Icon:    "fas fa-barcode",
-				Content: app.Text(c.Service.ServiceCode),
+				Title: "Service Code",
+				Icon:  "fas fa-barcode",
+				Content: app.Text(func() string {
+					if c.Service.ServiceCode != "" {
+						return c.Service.ServiceCode
+					}
+
+					return "Unknown service code"
+				}()),
 			},
 		),
 		app.Hr().Class("pf-c-divider pf-u-mb-md"),
 		app.Dl().Class("pf-c-description-list").Body(
 			&DefinitionComponent{
-				Title:   "Unauthorized Use Reported",
-				Icon:    "fas fa-exclamation-triangle",
-				Content: app.Text(c.Service.UnauthorizedUseReported),
+				Title: "Unauthorized Use Reported",
+				Icon:  "fas fa-exclamation-triangle",
+				Content: app.Text(func() string {
+					if c.Service.UnauthorizedUseReported != "" {
+						return c.Service.UnauthorizedUseReported
+					}
+
+					return "Not known"
+				}()),
 			},
 			&DefinitionComponent{
-				Title:   "Assignment Notes",
-				Icon:    "fas fa-sticky-note",
-				Content: app.Text(c.Service.AssignmentNotes),
+				Title: "Assignment Notes",
+				Icon:  "fas fa-sticky-note",
+				Content: app.Text(func() string {
+					if c.Service.AssignmentNotes != "" {
+						return c.Service.AssignmentNotes
+					}
+
+					return "No assignment notes"
+				}()),
 			},
 		),
 	)
