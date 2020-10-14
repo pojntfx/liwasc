cdaff6ad806476f2de628a1e88b7a2a257b7c11c
Felix Pojtinger <felix@pojtinger.com>
2020-10-14 22:40:16 +0000

    feat: Implement node metadata RPC

--- pkg/services/metadata_neo.go
+++ pkg/services/metadata_neo.go
@@ -2,11 +2,14 @@ package services
 
 import (
 	"context"
+	"log"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/pojntfx/liwasc/pkg/databases"
 	"github.com/pojntfx/liwasc/pkg/networking"
 	proto "github.com/pojntfx/liwasc/pkg/proto/generated"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type MetadataNeoService struct {
@@ -15,18 +18,21 @@ type MetadataNeoService struct {
 	subnets []string
 	device  string
 
-	interfaceInspector              *networking.InterfaceInspector
+	interfaceInspector *networking.InterfaceInspector
+
 	mac2vendorDatabase              *databases.MAC2VendorDatabase
 	serviceNamesPortNumbersDatabase *databases.ServiceNamesPortNumbersDatabase
 }
 
 func NewMetadataNeoService(
 	interfaceInspector *networking.InterfaceInspector,
+
 	mac2vendorDatabase *databases.MAC2VendorDatabase,
 	serviceNamesPortNumbersDatabase *databases.ServiceNamesPortNumbersDatabase,
 ) *MetadataNeoService {
 	return &MetadataNeoService{
-		interfaceInspector:              interfaceInspector,
+		interfaceInspector: interfaceInspector,
+
 		mac2vendorDatabase:              mac2vendorDatabase,
 		serviceNamesPortNumbersDatabase: serviceNamesPortNumbersDatabase,
 	}
@@ -45,10 +51,36 @@ func (s *MetadataNeoService) Open() error {
 }
 
 func (s *MetadataNeoService) GetMetadataForScanner(context.Context, *empty.Empty) (*proto.ScannerMetadataNeoMessage, error) {
-	protoScannerMetadataNeoMessage := &proto.ScannerMetadataNeoMessage{
+	protoScannerMetadataMessage := &proto.ScannerMetadataNeoMessage{
 		Subnets: s.subnets,
 		Device:  s.device,
 	}
 
-	return protoScannerMetadataNeoMessage, nil
+	return protoScannerMetadataMessage, nil
+}
+
+func (s *MetadataNeoService) GetMetadataForNode(_ context.Context, nodeReferenceMessage *proto.NodeMetadataReferenceNeoMessage) (*proto.NodeMetadataNeoMessage, error) {
+	dbNodeMetadata, err := s.mac2vendorDatabase.GetVendor(nodeReferenceMessage.GetMACAddress())
+	if err != nil {
+		log.Printf("could not find node %v in DB: %v\n", nodeReferenceMessage.GetMACAddress(), err)
+
+		return nil, status.Errorf(codes.NotFound, "could not find node in DB")
+	}
+
+	protoNodeMetadataMessage := &proto.NodeMetadataNeoMessage{
+		Address:      dbNodeMetadata.Address.String,
+		MACAddress:   nodeReferenceMessage.GetMACAddress(),
+		Organization: dbNodeMetadata.Organization.String,
+		Registry:     dbNodeMetadata.Registry,
+		Vendor:       dbNodeMetadata.Vendor.String,
+		Visible: func() bool {
+			if dbNodeMetadata.Visibility == 1 {
+				return true
+			}
+
+			return false
+		}(),
+	}
+
+	return protoNodeMetadataMessage, nil
 }
