76e57e300a5248e54df8371c3ffdac644df528f3
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-10-14 23:36:43 +0000

    feat: Add API definition for wake service

---
+++ pkg/sql/node_wake_neo.sql
@@ -0,0 +1,10 @@
+-- Remove old tables if they exist
+drop table if exists node_wake;
+-- Create new tables
+create table node_wake (
+    id integer not null primary key,
+    created_at date not null,
+    done integer not null,
+    mac_address text not null,
+    powered_on integer not null
+);
