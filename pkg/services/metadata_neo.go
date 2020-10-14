195fb81d0a77ead2af9e75a26c83b3fb0fd6a593
Felix Pojtinger <felix@pojtinger.com>
2020-10-14 23:05:16 +0000

    feat: Implement port metadata RPC

--- pkg/services/metadata_neo.go
+++ pkg/services/metadata_neo.go
@@ -2,7 +2,9 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/pojntfx/liwasc/pkg/databases"
@@ -59,17 +61,17 @@ func (s *MetadataNeoService) GetMetadataForScanner(context.Context, *empty.Empty
 	return protoScannerMetadataMessage, nil
 }
 
-func (s *MetadataNeoService) GetMetadataForNode(_ context.Context, nodeReferenceMessage *proto.NodeMetadataReferenceNeoMessage) (*proto.NodeMetadataNeoMessage, error) {
-	dbNodeMetadata, err := s.mac2vendorDatabase.GetVendor(nodeReferenceMessage.GetMACAddress())
+func (s *MetadataNeoService) GetMetadataForNode(_ context.Context, nodeMetadataReferenceMessage *proto.NodeMetadataReferenceNeoMessage) (*proto.NodeMetadataNeoMessage, error) {
+	dbNodeMetadata, err := s.mac2vendorDatabase.GetVendor(nodeMetadataReferenceMessage.GetMACAddress())
 	if err != nil {
-		log.Printf("could not find node %v in DB: %v\n", nodeReferenceMessage.GetMACAddress(), err)
+		log.Printf("could not find node %v in DB: %v\n", nodeMetadataReferenceMessage.GetMACAddress(), err)
 
 		return nil, status.Errorf(codes.NotFound, "could not find node in DB")
 	}
 
 	protoNodeMetadataMessage := &proto.NodeMetadataNeoMessage{
 		Address:      dbNodeMetadata.Address.String,
-		MACAddress:   nodeReferenceMessage.GetMACAddress(),
+		MACAddress:   nodeMetadataReferenceMessage.GetMACAddress(),
 		Organization: dbNodeMetadata.Organization.String,
 		Registry:     dbNodeMetadata.Registry,
 		Vendor:       dbNodeMetadata.Vendor.String,
@@ -83,4 +85,37 @@ func (s *MetadataNeoService) GetMetadataForNode(_ context.Context, nodeReference
 	}
 
 	return protoNodeMetadataMessage, nil
+}
+
+func (s *MetadataNeoService) GetMetadataForPort(_ context.Context, portMetadataReferenceMessage *proto.PortMetadataReferenceNeoMessage) (*proto.PortMetadataNeoMessage, error) {
+	dbPortMetadata, err := s.serviceNamesPortNumbersDatabase.GetService(int(portMetadataReferenceMessage.GetPortNumber()), portMetadataReferenceMessage.GetTransportProtocol())
+	if err != nil || (dbPortMetadata != nil && len(dbPortMetadata) == 0) {
+		log.Printf("could not find port %v in DB: %v\n", fmt.Sprintf("%v/%v", portMetadataReferenceMessage.GetPortNumber(), portMetadataReferenceMessage.GetTransportProtocol()), err)
+
+		return nil, status.Errorf(codes.NotFound, "could not find port in DB")
+	}
+
+	portNumber, err := strconv.Atoi(dbPortMetadata[0].PortNumber)
+	if err != nil {
+		log.Printf("could not find valid port number for port %v in DB: %v\n", fmt.Sprintf("%v/%v", portMetadataReferenceMessage.GetPortNumber(), portMetadataReferenceMessage.GetTransportProtocol()), err)
+
+		return nil, status.Errorf(codes.Unknown, "could not find valid port number in DB")
+	}
+
+	protoPortMetadataMessage := &proto.PortMetadataNeoMessage{
+		Assignee:                dbPortMetadata[0].Assignee,
+		AssignmentNotes:         dbPortMetadata[0].AssignmentNotes,
+		Contact:                 dbPortMetadata[0].Contact,
+		Description:             dbPortMetadata[0].Description,
+		ModificationDate:        dbPortMetadata[0].ModificationDate,
+		PortNumber:              int64(portNumber),
+		Reference:               dbPortMetadata[0].Reference,
+		RegistrationDate:        dbPortMetadata[0].RegistrationDate,
+		ServiceCode:             dbPortMetadata[0].ServiceCode,
+		ServiceName:             dbPortMetadata[0].ServiceName,
+		TransportProtocol:       dbPortMetadata[0].TransportProtocol,
+		UnauthorizedUseReported: dbPortMetadata[0].UnauthorizedUseReported,
+	}
+
+	return protoPortMetadataMessage, nil
 }
