18afca02154627c8945794605c321b332f44755a
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-10-14 22:40:16 +0000

    feat: Implement node metadata RPC

--- pkg/services/node_and_port_scan.go
+++ pkg/services/node_and_port_scan.go
@@ -58,7 +58,7 @@ func NewNodeAndPortScanPortService(
 	}
 }
 
-func (s *NodeAndPortScanPortService) StartNodeScan(ctx context.Context, nodeScanStartMessage *proto.NodeScanStartNeoMessage) (*proto.NodeScanNeoMessage, error) {
+func (s *NodeAndPortScanPortService) StartNodeScan(_ context.Context, nodeScanStartMessage *proto.NodeScanStartNeoMessage) (*proto.NodeScanNeoMessage, error) {
 	// Create and broadcast node scan in DB
 	dbNodeScan := &models.NodeScan{}
 	if err := s.nodeAndPortScanDatabase.CreateNodeScan(dbNodeScan); err != nil {
