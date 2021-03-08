1bec422caa8344a3670bb87a52cd0ef61b0bd099
Felix Pojtinger <felix@pojtinger.com>
2021-03-08 21:13:04 +0000

    feat: Start action implementation

--- pkg/components/experimental/actions.go
+++ pkg/components/experimental/actions.go
@@ -1,17 +1,78 @@
 package experimental
 
-import "github.com/maxence-charriere/go-app/v7/pkg/app"
+import (
+	"strconv"
+
+	"github.com/maxence-charriere/go-app/v7/pkg/app"
+	"github.com/pojntfx/liwasc-frontend-web/pkg/components/experimental/helpers"
+)
 
 type ActionsComponent struct {
 	app.Compo
 
+	nodeScanTimeout int64
+	portScanTimeout int64
+	macAddress      string
+
 	TriggerNetworkScan func(nodeScanTimeout int64, portScanTimeout int64, macAddress string)
 }
 
+const (
+	nodeScanTimeoutName = "nodeScanTimeout"
+)
+
 func (c *ActionsComponent) Render() app.UI {
 	return app.Div().Body(
-		app.Button().Text("Trigger network scan").OnClick(func(ctx app.Context, e app.Event) {
-			c.TriggerNetworkScan(500, 50, "")
+		app.Form().Body(
+			app.
+				Label().
+				For(nodeScanTimeoutName).
+				Text("Node Scan Timeout (in ms)"),
+			&helpers.Controlled{
+				Component: app.
+					Input().
+					Name(nodeScanTimeoutName).
+					Type("number").
+					Required(true).
+					Min(1).
+					Step(1).
+					Placeholder("500").
+					OnInput(func(ctx app.Context, e app.Event) {
+						v, err := strconv.Atoi(ctx.JSSrc.Get("value").String())
+						if err != nil {
+							panic(err)
+						}
+
+						c.nodeScanTimeout = int64(v)
+					}),
+				Value: c.nodeScanTimeout,
+			},
+			app.
+				Input().
+				Type("submit").
+				Value("Trigger network scan"),
+		).OnSubmit(func(ctx app.Context, e app.Event) {
+			e.PreventDefault()
+
+			go c.TriggerNetworkScan(c.nodeScanTimeout, c.portScanTimeout, c.macAddress)
+
+			c.clearNetworkScanForm()
 		}),
 	)
+}
+
+func (c *ActionsComponent) clearNetworkScanForm() {
+	// Reset the values
+	c.nodeScanTimeout = 0
+	c.portScanTimeout = 0
+	c.macAddress = ""
+
+	c.Update()
+}
+
+func (c *ActionsComponent) OnMount(context app.Context) {
+	// Initialize form state
+	c.nodeScanTimeout = 0
+	c.portScanTimeout = 0
+	c.macAddress = ""
 }
