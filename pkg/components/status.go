a8f761c775f1d4e2f4c3f8a3f8d09d87406eb659
Felix Pojtinger <felix@pojtinger.com>
2021-03-15 18:52:31 +0000

    feat: Use Patternfly alert for error output

--- pkg/components/status.go
+++ pkg/components/status.go
@@ -16,10 +16,47 @@ func (c *StatusComponent) Render() app.UI {
 		errorMessage = c.Error.Error()
 	}
 
-	return app.If(c.Error != nil, app.Div().Body(
-		app.P().Text(errorMessage),
-		app.If(c.Recover != nil, app.Button().Text("Recover").OnClick(func(ctx app.Context, e app.Event) {
-			c.Recover()
-		})),
-	)).Else(app.Span())
+	return app.If(c.Error != nil, app.Div().
+		Class("pf-c-alert pf-m-danger").
+		Aria("label", "Error").
+		Body(
+			app.Div().
+				Class("pf-c-alert__icon").
+				Body(
+					app.I().
+						Class("fas fa-fw fa-exclamation-circle").
+						Aria("hidden", true),
+				),
+			app.P().
+				Class("pf-c-alert__title").
+				Body(
+					app.Strong().Body(
+						app.Span().
+							Class("pf-screen-reader").
+							Text("Error"),
+					),
+					app.Text("Error"),
+				),
+			app.Div().
+				Class("pf-c-alert__description").
+				Body(
+					app.P().Body(
+						app.Code().
+							Text(errorMessage),
+					),
+				),
+			app.If(c.Recover != nil,
+				app.Div().
+					Class("pf-c-alert__action-group").
+					Body(
+						app.Button().
+							Class("pf-c-button pf-m-link pf-m-inline").
+							Type("button").
+							Text("Recover").
+							OnClick(func(ctx app.Context, e app.Event) {
+								c.Recover()
+							}),
+					),
+			),
+		)).Else(app.Span())
 }
