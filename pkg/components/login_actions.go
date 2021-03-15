ac2606ba10153b1e8a9091964b7c478f1f2480f6
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-03-15 19:09:16 +0000

    feat: Style logout button

--- pkg/components/login_actions.go
+++ pkg/components/login_actions.go
@@ -11,7 +11,11 @@ type LoginActionsComponent struct {
 }
 
 func (c *LoginActionsComponent) Render() app.UI {
-	return app.Button().Text("Logout").OnClick(func(ctx app.Context, e app.Event) {
-		go c.Logout()
-	})
+	return app.Button().
+		Text("Logout").
+		Class("pf-c-button pf-m-primary pf-m-control").
+		Type("button").
+		OnClick(func(ctx app.Context, e app.Event) {
+			go c.Logout()
+		})
 }
