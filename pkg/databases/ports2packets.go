71b94c1fbd395556adc0b41301cf35bf08cd1818
Felix Pojtinger <felix@pojtinger.com>
2021-03-27 23:44:32 +0100

    feat: Download the ports2packets database if it doesn't exist

--- pkg/databases/ports2packets.go
+++ pkg/databases/ports2packets.go
@@ -3,7 +3,11 @@ package databases
 import (
 	"encoding/base64"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/jszwec/csvutil"
 )
@@ -19,15 +23,44 @@ type Packet struct {
 }
 
 type Ports2PacketDatabase struct {
-	dbPath  string
-	packets map[int]*Packet
+	dbPath    string
+	sourceURL string
+	packets   map[int]*Packet
 }
 
-func NewPorts2PacketDatabase(dbPath string) *Ports2PacketDatabase {
-	return &Ports2PacketDatabase{dbPath, make(map[int]*Packet)}
+func NewPorts2PacketDatabase(dbPath string, sourceURL string) *Ports2PacketDatabase {
+	return &Ports2PacketDatabase{dbPath, sourceURL, make(map[int]*Packet)}
 }
 
 func (d *Ports2PacketDatabase) Open() error {
+	// If CSV file does not exist, download & create it
+	if _, err := os.Stat(d.dbPath); os.IsNotExist(err) {
+		// Create leading directories
+		leadingDir, _ := filepath.Split(d.dbPath)
+		if err := os.MkdirAll(leadingDir, os.ModeDir); err != nil {
+			return err
+		}
+
+		// Create file
+		out, err := os.Create(d.dbPath)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+
+		// Download file
+		res, err := http.Get(d.sourceURL)
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		// Write to file
+		if _, err := io.Copy(out, res.Body); err != nil {
+			return err
+		}
+	}
+
 	// Read CSV file
 	contents, err := ioutil.ReadFile(d.dbPath)
 	if err != nil {
