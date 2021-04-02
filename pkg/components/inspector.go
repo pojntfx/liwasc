e2dd02ca22595539408264ca284904684b8cc757
Felix Pojtinger <felix@pojtinger.com>
2021-04-02 22:40:22 +0000

    feat: Start port inspector implementation

--- pkg/components/inspector.go
+++ pkg/components/inspector.go
@@ -89,100 +89,115 @@ func (c *Inspector) Render() app.UI {
 										app.Span().
 											Text(fmt.Sprintf("Node %v", c.Node.MACAddress)),
 									).Else(
-										app.Span().
-											Text(fmt.Sprintf("Port %v", GetPortID(selectedPort))),
+										app.Div().
+											Body(
+												app.Button().
+													Class("pf-c-button pf-m-plain pf-u-mr-md").
+													Type("button").
+													Aria("label", "Close port inspector").
+													OnClick(func(ctx app.Context, e app.Event) {
+														c.SetSelectedPort("")
+													}).Body(
+													app.I().Class("fas fa-arrow-left").Aria("hidden", true),
+												),
+												app.Span().
+													Text(fmt.Sprintf("Port %v", GetPortID(selectedPort))),
+											),
 									),
 									app.Div().
 										Class("pf-c-drawer__actions").
 										Body(
-											app.Label().
-												Class("pf-c-switch pf-x-c-tooltip-wrapper pf-x-c-power-switch pf-u-mr-md").
-												For("selected-node-inspector-power").
-												Body(
-													app.If(
-														c.Node.PoweredOn,
-														app.Div().
-															Class("pf-c-tooltip pf-x-c-tooltip pf-x-c-tooltip--bottom pf-m-bottom").
-															Aria("role", "tooltip").
-															Body(
-																app.Div().
-																	Class("pf-c-tooltip__arrow"),
-																app.Div().
-																	Class("pf-c-tooltip__content").
-																	Text("To turn this node off, please do so manually."),
-															),
-													),
-													&Controlled{
-														Component: app.Input().
-															Class("pf-c-switch__input").
-															ID("selected-node-inspector-power").
-															Aria("label", "Node is off").
-															Name("selected-node-inspector-power").
-															Type("checkbox").
-															Checked(c.Node.PoweredOn).
-															Disabled(c.Node.PoweredOn).
-															OnClick(func(ctx app.Context, e app.Event) {
-																e.Call("stopPropagation")
-
-																c.StartNodeWake()
-															}),
-														Properties: map[string]interface{}{
-															"checked":  c.Node.PoweredOn,
-															"disabled": c.Node.PoweredOn,
-														},
-													},
-													app.Span().
-														Class("pf-c-switch__toggle").
-														Body(
-															app.Span().
-																Class("pf-c-switch__toggle-icon").
+											app.If(
+												c.SelectedPort == "",
+												app.Label().
+													Class("pf-c-switch pf-x-c-tooltip-wrapper pf-x-c-power-switch pf-u-mr-md").
+													For("selected-node-inspector-power").
+													Body(
+														app.If(
+															c.Node.PoweredOn,
+															app.Div().
+																Class("pf-c-tooltip pf-x-c-tooltip pf-x-c-tooltip--bottom pf-m-bottom").
+																Aria("role", "tooltip").
 																Body(
-																	app.I().
-																		Class("fas fa-lightbulb").
-																		Aria("hidden", true),
+																	app.Div().
+																		Class("pf-c-tooltip__arrow"),
+																	app.Div().
+																		Class("pf-c-tooltip__content").
+																		Text("To turn this node off, please do so manually."),
 																),
 														),
-													app.Span().
-														Class("pf-c-switch__label pf-m-on pf-l-flex pf-m-justify-content-center pf-m-align-items-center").
-														ID("selected-node-inspector-power-on").
-														Aria("hidden", true).
-														Body(
-															app.If(
-																c.Node.NodeWakeRunning,
+														&Controlled{
+															Component: app.Input().
+																Class("pf-c-switch__input").
+																ID("selected-node-inspector-power").
+																Aria("label", "Node is off").
+																Name("selected-node-inspector-power").
+																Type("checkbox").
+																Checked(c.Node.PoweredOn).
+																Disabled(c.Node.PoweredOn).
+																OnClick(func(ctx app.Context, e app.Event) {
+																	e.Call("stopPropagation")
+
+																	c.StartNodeWake()
+																}),
+															Properties: map[string]interface{}{
+																"checked":  c.Node.PoweredOn,
+																"disabled": c.Node.PoweredOn,
+															},
+														},
+														app.Span().
+															Class("pf-c-switch__toggle").
+															Body(
 																app.Span().
-																	Class("pf-c-spinner pf-m-md").
-																	Aria("role", "progressbar").
-																	Aria("valuetext", "Loading...").
+																	Class("pf-c-switch__toggle-icon").
 																	Body(
-																		app.Span().Class("pf-c-spinner__clipper"),
-																		app.Span().Class("pf-c-spinner__lead-ball"),
-																		app.Span().Class("pf-c-spinner__tail-ball"),
+																		app.I().
+																			Class("fas fa-lightbulb").
+																			Aria("hidden", true),
 																	),
-															).Else(
-																app.Text("On"),
 															),
-														),
-													app.Span().
-														Class("pf-c-switch__label pf-m-off pf-l-flex pf-m-justify-content-center pf-m-align-items-center").
-														ID("selected-node-inspector-power-off").
-														Aria("hidden", true).
-														Body(
-															app.If(
-																c.Node.NodeWakeRunning,
-																app.Span().
-																	Class("pf-c-spinner pf-m-md").
-																	Aria("role", "progressbar").
-																	Aria("valuetext", "Loading...").
-																	Body(
-																		app.Span().Class("pf-c-spinner__clipper"),
-																		app.Span().Class("pf-c-spinner__lead-ball"),
-																		app.Span().Class("pf-c-spinner__tail-ball"),
-																	),
-															).Else(
-																app.Text("Off"),
+														app.Span().
+															Class("pf-c-switch__label pf-m-on pf-l-flex pf-m-justify-content-center pf-m-align-items-center").
+															ID("selected-node-inspector-power-on").
+															Aria("hidden", true).
+															Body(
+																app.If(
+																	c.Node.NodeWakeRunning,
+																	app.Span().
+																		Class("pf-c-spinner pf-m-md").
+																		Aria("role", "progressbar").
+																		Aria("valuetext", "Loading...").
+																		Body(
+																			app.Span().Class("pf-c-spinner__clipper"),
+																			app.Span().Class("pf-c-spinner__lead-ball"),
+																			app.Span().Class("pf-c-spinner__tail-ball"),
+																		),
+																).Else(
+																	app.Text("On"),
+																),
 															),
-														),
-												),
+														app.Span().
+															Class("pf-c-switch__label pf-m-off pf-l-flex pf-m-justify-content-center pf-m-align-items-center").
+															ID("selected-node-inspector-power-off").
+															Aria("hidden", true).
+															Body(
+																app.If(
+																	c.Node.NodeWakeRunning,
+																	app.Span().
+																		Class("pf-c-spinner pf-m-md").
+																		Aria("role", "progressbar").
+																		Aria("valuetext", "Loading...").
+																		Body(
+																			app.Span().Class("pf-c-spinner__clipper"),
+																			app.Span().Class("pf-c-spinner__lead-ball"),
+																			app.Span().Class("pf-c-spinner__tail-ball"),
+																		),
+																).Else(
+																	app.Text("Off"),
+																),
+															),
+													),
+											),
 											app.Div().
 												Class("pf-c-drawer__close").
 												Body(
@@ -202,95 +217,99 @@ func (c *Inspector) Render() app.UI {
 					app.Div().
 						Class("pf-c-drawer__body").
 						Body(
-							app.Dl().
-								Class("pf-c-description-list pf-m-2-col").
-								Body(
-									&Property{
-										Key:   "IP Address",
-										Value: c.Node.IPAddress,
-									},
-									&Property{
-										Key:   "Vendor",
-										Value: c.Node.Vendor,
-									},
-									&Property{
-										Key:   "Registry",
-										Value: c.Node.Registry,
-									},
-									&Property{
-										Key:   "Organization",
-										Value: c.Node.Organization,
-									},
-									&Property{
-										Key:   "Address",
-										Value: c.Node.Address,
-									},
-									&Property{
-										Key:   "Visible",
-										Value: fmt.Sprintf("%v", c.Node.Visible),
-									},
-								),
-							&ProgressButton{
-								Loading:   c.Node.PortScanRunning,
-								Icon:      "fas fa-sync",
-								Text:      "Trigger Port Scan",
-								Secondary: true,
-								Classes:   "pf-u-w-100 pf-u-mt-lg",
-
-								OnClick: func(ctx app.Context, e app.Event) {
-									e.Call("stopPropagation")
-
-									c.TriggerNetworkScan()
-								},
-							},
-							app.Div().
-								Class("pf-c-input-group pf-u-mt-lg").
-								Body(
-									&Controlled{
-										Component: app.
-											Input().
-											Type("search").
-											Placeholder("Service name or port number").
-											Class("pf-c-form-control").
-											Aria("label", "Service name or port number").
-											OnInput(func(ctx app.Context, e app.Event) {
-												c.SetPortFilter(ctx.JSSrc.Get("value").String())
-											}),
-										Properties: map[string]interface{}{
-											"value": c.PortFilter,
-										},
-									},
-									app.Button().
-										Class("pf-c-button pf-m-control").
-										Type("button").
-										Aria("label", "Search button for service name or port number").Body(
-										app.I().
-											Class("fas fa-search").
-											Aria("hidden", true),
-									),
-								),
 							app.If(
-								len(filteredPorts) > 0,
-								&PortSelectionList{
-									Ports:           filteredPorts,
-									SelectedPort:    c.SelectedPort,
-									SetSelectedPort: c.SetSelectedPort,
+								c.SelectedPort == "",
+								app.Dl().
+									Class("pf-c-description-list pf-m-2-col").
+									Body(
+										&Property{
+											Key:   "IP Address",
+											Value: c.Node.IPAddress,
+										},
+										&Property{
+											Key:   "Vendor",
+											Value: c.Node.Vendor,
+										},
+										&Property{
+											Key:   "Registry",
+											Value: c.Node.Registry,
+										},
+										&Property{
+											Key:   "Organization",
+											Value: c.Node.Organization,
+										},
+										&Property{
+											Key:   "Address",
+											Value: c.Node.Address,
+										},
+										&Property{
+											Key:   "Visible",
+											Value: fmt.Sprintf("%v", c.Node.Visible),
+										},
+									),
+								&ProgressButton{
+									Loading:   c.Node.PortScanRunning,
+									Icon:      "fas fa-sync",
+									Text:      "Trigger Port Scan",
+									Secondary: true,
+									Classes:   "pf-u-w-100 pf-u-mt-lg",
+
+									OnClick: func(ctx app.Context, e app.Event) {
+										e.Call("stopPropagation")
+
+										c.TriggerNetworkScan()
+									},
 								},
-							).
-								ElseIf(
-									c.PortFilter != "",
-									app.Div().Class("pf-u-mt-lg").Text("No open ports found for this filter."),
+								app.Div().
+									Class("pf-c-input-group pf-u-mt-lg").
+									Body(
+										&Controlled{
+											Component: app.
+												Input().
+												Type("search").
+												Placeholder("Service name or port number").
+												Class("pf-c-form-control").
+												Aria("label", "Service name or port number").
+												OnInput(func(ctx app.Context, e app.Event) {
+													c.SetPortFilter(ctx.JSSrc.Get("value").String())
+												}),
+											Properties: map[string]interface{}{
+												"value": c.PortFilter,
+											},
+										},
+										app.Button().
+											Class("pf-c-button pf-m-control").
+											Type("button").
+											Aria("label", "Search button for service name or port number").Body(
+											app.I().
+												Class("fas fa-search").
+												Aria("hidden", true),
+										),
+									),
+								app.If(
+									len(filteredPorts) > 0,
+									&PortSelectionList{
+										Ports:           filteredPorts,
+										SelectedPort:    c.SelectedPort,
+										SetSelectedPort: c.SetSelectedPort,
+									},
 								).
-								ElseIf(
-									c.Node.PortScanRunning,
-									app.Div().Class("pf-u-mt-lg").Text("No open ports found yet."),
-								).
-								Else(
-									app.Div().Class("pf-u-mt-lg").Text("No open ports found."),
-								),
-							&JSONDisplay{
-								Object: c.Node,
-							},
+									ElseIf(
+										c.PortFilter != "",
+										app.Div().Class("pf-u-mt-lg").Text("No open ports found for this filter."),
+									).
+									ElseIf(
+										c.Node.PortScanRunning,
+										app.Div().Class("pf-u-mt-lg").Text("No open ports found yet."),
+									).
+									Else(
+										app.Div().Class("pf-u-mt-lg").Text("No open ports found."),
+									),
+							).Else(
+								&JSONDisplay{
+									Object: selectedPort,
+								},
+							),
 						),
 				),
 			),
