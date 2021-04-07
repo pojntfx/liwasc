c5ebec1615547d4f526a8f5a149015e661d48ce2
Felix Pojtinger <felix@pojtinger.com>
2021-04-07 19:23:14 +0000

    refactor: Decompose notification drawer

--- pkg/components/data_shell.go
+++ pkg/components/data_shell.go
@@ -79,6 +79,15 @@ func (c *DataShell) Render() app.UI {
 		}
 	}
 
+	// Gather notifications
+	notifications := []Notification{}
+	for _, event := range c.Network.Events {
+		notifications = append(notifications, Notification{
+			Message: event.Message,
+			Time:    event.Time.String(),
+		})
+	}
+
 	return app.Div().
 		Class("pf-u-h-100").
 		Body(
@@ -257,31 +266,9 @@ func (c *DataShell) Render() app.UI {
 													app.Div().
 														Class("pf-c-drawer__body pf-m-no-padding").
 														Body(
-															app.Div().
-																Class("pf-c-notification-drawer").
-																Body(
-																	app.Div().
-																		Class("pf-c-notification-drawer__header").
-																		Body(
-																			app.H1().
-																				Class("pf-c-notification-drawer__header-title").
-																				Text("Events"),
-																		),
-																	app.Div().Class("pf-c-notification-drawer__body").Body(
-																		app.Ul().Class("pf-c-notification-drawer__list").Body(
-																			app.Range(c.Network.Events).Slice(func(i int) app.UI {
-																				return app.Li().Class("pf-c-notification-drawer__list-item pf-m-read pf-m-info").Body(
-																					app.Div().Class("pf-c-notification-drawer__list-item-description").Text(
-																						c.Network.Events[len(c.Network.Events)-1-i].Message,
-																					),
-																					app.Div().Class("pf-c-notification-drawer__list-item-timestamp").Text(
-																						c.Network.Events[len(c.Network.Events)-1-i].Time,
-																					),
-																				)
-																			}),
-																		),
-																	),
-																),
+															&NotificationDrawer{
+																Notifications: notifications,
+															},
 														),
 												),
 										),
